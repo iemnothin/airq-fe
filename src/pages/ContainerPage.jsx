c6f6882b602d832f879b73b71c274d4f023741c7
iemnothin <okysaputraa10@gmail.com>
2025-11-27 10:41:16 +0700

    refactor nav

--- src/pages/ContainerPage.jsx
+++ src/pages/ContainerPage.jsx
@@ -1,35 +1,30 @@
 // src/pages/ContainerPage.jsx
-import { Link, Outlet } from "react-router-dom";
 import Navigation from "../components/Navigation";
-
-const MOBILE_LOGO = process.env.PUBLIC_URL + "/air-no-outline192.png";
-
-const MobileTopLogoBar = () => (
-  <div className="mobile-top-logo-bar d-block d-md-none text-center py-2">
-    <Link
-      to="/"
-      className="nav-item fw-bold text-decoration-none d-flex flex-column align-items-center">
-      <img src={MOBILE_LOGO} alt="AirQ Logo" style={{ width: "55px" }} />
-      <div className="logo-text">AirQ</div>
-    </Link>
-  </div>
-);
+import { Outlet, Link } from "react-router-dom";
 
 const ContainerPage = () => {
   return (
     <div className="d-flex">
-      {/* MOBILE LOGO BAR */}
-      <MobileTopLogoBar />
+      {/* Top Bar Logo khusus Mobile */}
+      <div className="mobile-top-logo-bar d-block d-md-none text-center">
+        <Link to="/" className="h-4 nav-item text-center mb-4 fw-bold text-decoration-none">
+          <img
+            src={process.env.PUBLIC_URL + "/air-no-outline192.png"}
+            alt="AirQ Logo"
+          />
+          <div className="logo-text">AirQ</div>
+        </Link>
+      </div>
 
-      {/* SIDEBAR */}
+      {/* Sidebar Navigation */}
       <Navigation className="flex-shrink-0" />
 
-      {/* MAIN PAGE CONTENT */}
-      <main className="flex-grow-1 container-fluid p-0">
+      {/* Page Content */}
+      <div className="flex-grow-1 container-fluid p-0">
         <div className="content-wrapper">
           <Outlet />
         </div>
-      </main>
+      </div>
     </div>
   );
 };
